1bad41892429c089d963ac10ec25354381ca108f
Gol4m <armored322@gmail.com>
2022-05-27 13:44:10 +0300

    4 lab update

--- 4lab_garden_interface/controler.py
+++
@@ -1,8 +0,0 @@
-import pygame_widgets
-from pygame_widgets.button import Button
-
-class Controller:
-    def __init__(self):
-        pass
-    
-    
--- 4lab_garden_interface/settings.py
+++
@@ -1,6 +0,0 @@
-WIDTH, HEIGHT = 900, 600
-BLACK = (0, 0, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0 )
-WHITE = (255, 255, 255)
-FONT = 'Comic Sans MS'
